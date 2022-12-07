e8fa29b18a16981eed3bf3efa8fde472ae2cfe3b
tiziano.cappai <tiziano.cappai@accenture.com>
2022-12-07 16:35:28 +0100

    add animation base cube, orbit  control, axishelper

--- three-js-app/src/components/base-cube/index.tsx
+++ three-js-app/src/components/base-cube/index.tsx
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { useEffect, useRef } from 'react';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import './style.css';
 
 function BaseCube() {
 	const canvasRef = useRef<any>();
@@ -11,21 +13,25 @@ function BaseCube() {
 		//set the camera
 
 		const fov = 75; // field of view
-		const aspect = 2; // the canvas default
+		const aspect = window.innerWidth / window.innerHeight; // the canvas default
 		const near = 0.1;
-		const far = 5;
+		const far = 100;
 
 		const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-		camera.position.z = 2;
+
+		//allow user to control scene using mouse
+		const orbit = new OrbitControls(camera, renderer.domElement);
+
+		camera.position.set(0, 2, 5);
+
+		// call this method after set the camera position
+		orbit.update();
 
 		// set a Scene
 		const scene = new THREE.Scene();
 
 		// create a box geometry
-		const boxWidth = 1;
-		const boxHeight = 1;
-		const boxDepth = 1;
-		const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+		const geometry = new THREE.BoxGeometry();
 
 		//create basic material
 		const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
@@ -36,11 +42,21 @@ function BaseCube() {
 		// aggiungiamo il cubo alla scena
 		scene.add(cube);
 
+		function animate() {
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+			renderer.render(scene, camera);
+		}
+		cube.rotation.x = 5;
+		cube.rotation.y = 2;
+
+		const axesHelper = new THREE.AxesHelper(5);
+		scene.add(axesHelper);
 		// renderizziamo scena e camera
-		renderer.render(scene, camera);
+		renderer.setAnimationLoop(animate);
 	}, []);
 
-	return <canvas ref={canvasRef}></canvas>;
+	return <canvas className='base-cube-container' ref={canvasRef}></canvas>;
 }
 
 export default BaseCube;
